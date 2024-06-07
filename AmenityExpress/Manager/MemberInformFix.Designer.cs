23443d0afe2f79445fa2d74ec56f2f68196493a9
이준민JunminLee <123wnsalsqld1@gmail.com>
2024-06-08 04:49:18 +0900

    MemberInformFix
    회원정보수정(회원) 구현

---
+++ AmenityExpress/Manager/MemberInformFix.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace AmenityExpress
+{
+    partial class MemberInformFix
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.female_Rbtn = new System.Windows.Forms.RadioButton();
+            this.male_Rbtn = new System.Windows.Forms.RadioButton();
+            this.birth_lbl = new System.Windows.Forms.Label();
+            this.Birth_txt = new System.Windows.Forms.TextBox();
+            this.sex_lbl = new System.Windows.Forms.Label();
+            this.Fix_btn = new System.Windows.Forms.Button();
+            this.Tel_txt = new System.Windows.Forms.TextBox();
+            this.Email_txt = new System.Windows.Forms.TextBox();
+            this.PWC_txt = new System.Windows.Forms.TextBox();
+            this.PW_txt = new System.Windows.Forms.TextBox();
+            this.ID_txt = new System.Windows.Forms.TextBox();
+            this.Name_txt = new System.Windows.Forms.TextBox();
+            this.Email_lbl = new System.Windows.Forms.Label();
+            this.Tel_lbl = new System.Windows.Forms.Label();
+            this.PWC_lbl = new System.Windows.Forms.Label();
+            this.PW_lbl = new System.Windows.Forms.Label();
+            this.ID_lbl = new System.Windows.Forms.Label();
+            this.name_lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // female_Rbtn
+            // 
+            this.female_Rbtn.AutoSize = true;
+            this.female_Rbtn.Location = new System.Drawing.Point(184, 261);
+            this.female_Rbtn.Name = "female_Rbtn";
+            this.female_Rbtn.Size = new System.Drawing.Size(35, 16);
+            this.female_Rbtn.TabIndex = 41;
+            this.female_Rbtn.TabStop = true;
+            this.female_Rbtn.Text = "여";
+            this.female_Rbtn.UseVisualStyleBackColor = true;
+            // 
+            // male_Rbtn
+            // 
+            this.male_Rbtn.AutoSize = true;
+            this.male_Rbtn.Location = new System.Drawing.Point(118, 261);
+            this.male_Rbtn.Name = "male_Rbtn";
+            this.male_Rbtn.Size = new System.Drawing.Size(35, 16);
+            this.male_Rbtn.TabIndex = 40;
+            this.male_Rbtn.TabStop = true;
+            this.male_Rbtn.Text = "남";
+            this.male_Rbtn.UseVisualStyleBackColor = true;
+            // 
+            // birth_lbl
+            // 
+            this.birth_lbl.AutoSize = true;
+            this.birth_lbl.Location = new System.Drawing.Point(33, 302);
+            this.birth_lbl.Name = "birth_lbl";
+            this.birth_lbl.Size = new System.Drawing.Size(53, 12);
+            this.birth_lbl.TabIndex = 39;
+            this.birth_lbl.Text = "생년월일";
+            // 
+            // Birth_txt
+            // 
+            this.Birth_txt.Location = new System.Drawing.Point(115, 299);
+            this.Birth_txt.Name = "Birth_txt";
+            this.Birth_txt.Size = new System.Drawing.Size(142, 21);
+            this.Birth_txt.TabIndex = 38;
+            // 
+            // sex_lbl
+            // 
+            this.sex_lbl.AutoSize = true;
+            this.sex_lbl.Location = new System.Drawing.Point(33, 263);
+            this.sex_lbl.Name = "sex_lbl";
+            this.sex_lbl.Size = new System.Drawing.Size(29, 12);
+            this.sex_lbl.TabIndex = 37;
+            this.sex_lbl.Text = "생일";
+            // 
+            // Fix_btn
+            // 
+            this.Fix_btn.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Fix_btn.Location = new System.Drawing.Point(35, 341);
+            this.Fix_btn.Name = "Fix_btn";
+            this.Fix_btn.Size = new System.Drawing.Size(296, 31);
+            this.Fix_btn.TabIndex = 36;
+            this.Fix_btn.Text = "회원정보수정";
+            this.Fix_btn.UseVisualStyleBackColor = true;
+            this.Fix_btn.Click += new System.EventHandler(this.Fix_btn_Click);
+            // 
+            // Tel_txt
+            // 
+            this.Tel_txt.Location = new System.Drawing.Point(118, 224);
+            this.Tel_txt.Name = "Tel_txt";
+            this.Tel_txt.Size = new System.Drawing.Size(142, 21);
+            this.Tel_txt.TabIndex = 32;
+            // 
+            // Email_txt
+            // 
+            this.Email_txt.Location = new System.Drawing.Point(118, 190);
+            this.Email_txt.Name = "Email_txt";
+            this.Email_txt.Size = new System.Drawing.Size(141, 21);
+            this.Email_txt.TabIndex = 31;
+            // 
+            // PWC_txt
+            // 
+            this.PWC_txt.Location = new System.Drawing.Point(118, 153);
+            this.PWC_txt.Name = "PWC_txt";
+            this.PWC_txt.Size = new System.Drawing.Size(139, 21);
+            this.PWC_txt.TabIndex = 30;
+            // 
+            // PW_txt
+            // 
+            this.PW_txt.Location = new System.Drawing.Point(118, 113);
+            this.PW_txt.Name = "PW_txt";
+            this.PW_txt.Size = new System.Drawing.Size(141, 21);
+            this.PW_txt.TabIndex = 29;
+            // 
+            // ID_txt
+            // 
+            this.ID_txt.Location = new System.Drawing.Point(118, 74);
+            this.ID_txt.Name = "ID_txt";
+            this.ID_txt.Size = new System.Drawing.Size(141, 21);
+            this.ID_txt.TabIndex = 28;
+            // 
+            // Name_txt
+            // 
+            this.Name_txt.Location = new System.Drawing.Point(116, 31);
+            this.Name_txt.Name = "Name_txt";
+            this.Name_txt.Size = new System.Drawing.Size(141, 21);
+            this.Name_txt.TabIndex = 27;
+            // 
+            // Email_lbl
+            // 
+            this.Email_lbl.AutoSize = true;
+            this.Email_lbl.Location = new System.Drawing.Point(33, 193);
+            this.Email_lbl.Name = "Email_lbl";
+            this.Email_lbl.Size = new System.Drawing.Size(41, 12);
+            this.Email_lbl.TabIndex = 26;
+            this.Email_lbl.Text = "이메일";
+            // 
+            // Tel_lbl
+            // 
+            this.Tel_lbl.AutoSize = true;
+            this.Tel_lbl.Location = new System.Drawing.Point(33, 227);
+            this.Tel_lbl.Name = "Tel_lbl";
+            this.Tel_lbl.Size = new System.Drawing.Size(53, 12);
+            this.Tel_lbl.TabIndex = 25;
+            this.Tel_lbl.Text = "전화번호";
+            // 
+            // PWC_lbl
+            // 
+            this.PWC_lbl.AutoSize = true;
+            this.PWC_lbl.Location = new System.Drawing.Point(33, 156);
+            this.PWC_lbl.Name = "PWC_lbl";
+            this.PWC_lbl.Size = new System.Drawing.Size(81, 12);
+            this.PWC_lbl.TabIndex = 24;
+            this.PWC_lbl.Text = "비밀번호 확인";
+            // 
+            // PW_lbl
+            // 
+            this.PW_lbl.AutoSize = true;
+            this.PW_lbl.Location = new System.Drawing.Point(33, 122);
+            this.PW_lbl.Name = "PW_lbl";
+            this.PW_lbl.Size = new System.Drawing.Size(53, 12);
+            this.PW_lbl.TabIndex = 23;
+            this.PW_lbl.Text = "비밀번호";
+            // 
+            // ID_lbl
+            // 
+            this.ID_lbl.AutoSize = true;
+            this.ID_lbl.Location = new System.Drawing.Point(33, 83);
+            this.ID_lbl.Name = "ID_lbl";
+            this.ID_lbl.Size = new System.Drawing.Size(41, 12);
+            this.ID_lbl.TabIndex = 22;
+            this.ID_lbl.Text = "아이디";
+            // 
+            // name_lbl
+            // 
+            this.name_lbl.AutoSize = true;
+            this.name_lbl.Location = new System.Drawing.Point(33, 40);
+            this.name_lbl.Name = "name_lbl";
+            this.name_lbl.Size = new System.Drawing.Size(29, 12);
+            this.name_lbl.TabIndex = 21;
+            this.name_lbl.Text = "이름";
+            // 
+            // MemberInformFix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 392);
+            this.Controls.Add(this.female_Rbtn);
+            this.Controls.Add(this.male_Rbtn);
+            this.Controls.Add(this.birth_lbl);
+            this.Controls.Add(this.Birth_txt);
+            this.Controls.Add(this.sex_lbl);
+            this.Controls.Add(this.Fix_btn);
+            this.Controls.Add(this.Tel_txt);
+            this.Controls.Add(this.Email_txt);
+            this.Controls.Add(this.PWC_txt);
+            this.Controls.Add(this.PW_txt);
+            this.Controls.Add(this.ID_txt);
+            this.Controls.Add(this.Name_txt);
+            this.Controls.Add(this.Email_lbl);
+            this.Controls.Add(this.Tel_lbl);
+            this.Controls.Add(this.PWC_lbl);
+            this.Controls.Add(this.PW_lbl);
+            this.Controls.Add(this.ID_lbl);
+            this.Controls.Add(this.name_lbl);
+            this.Name = "MemberInformFix";
+            this.Text = "MemberInformFix";
+            this.Load += new System.EventHandler(this.MemberInformFix_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton female_Rbtn;
+        private System.Windows.Forms.RadioButton male_Rbtn;
+        private System.Windows.Forms.Label birth_lbl;
+        private System.Windows.Forms.TextBox Birth_txt;
+        private System.Windows.Forms.Label sex_lbl;
+        private System.Windows.Forms.Button Fix_btn;
+        private System.Windows.Forms.TextBox Tel_txt;
+        private System.Windows.Forms.TextBox Email_txt;
+        private System.Windows.Forms.TextBox PWC_txt;
+        private System.Windows.Forms.TextBox PW_txt;
+        private System.Windows.Forms.TextBox ID_txt;
+        private System.Windows.Forms.TextBox Name_txt;
+        private System.Windows.Forms.Label Email_lbl;
+        private System.Windows.Forms.Label Tel_lbl;
+        private System.Windows.Forms.Label PWC_lbl;
+        private System.Windows.Forms.Label PW_lbl;
+        private System.Windows.Forms.Label ID_lbl;
+        private System.Windows.Forms.Label name_lbl;
+    }
+}
